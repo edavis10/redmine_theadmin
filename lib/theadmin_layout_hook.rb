ea6a3e37131d79e1aaafa00566cc9841c991574b
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-28 16:20:42 -0700

    Changed the string to use Redmine i18n helper

--- lib/theadmin_layout_hook.rb
+++ lib/theadmin_layout_hook.rb
@@ -1,5 +1,5 @@
 class TheadminLayoutHook < Redmine::Hook::ViewListener
   def view_layouts_base_content(context = {})
-    return 'This is a string from a plugin hook'
+    return l(:field_homepage)
   end
 end
