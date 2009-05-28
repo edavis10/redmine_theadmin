b8156f65f0d21c0de71c6ab6a167defb416133a8
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-28 15:45:37 -0700

    Configured init.rb and added i18n files

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_theadmin do
+  name 'theAdmin plugin'
+  author 'Eric Davis'
+  url 'http://theadmin.org'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "This is a Redmine plugin that is used to provide examples for http://theadmin.org"
+  version '0.0.0'
+end
