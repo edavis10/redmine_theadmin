684c82f6617600eea204489ce674c5fb97f5d53f
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-28 16:33:23 -0700

    Changed the issue hook to show how to use the context data and the "h" helper

--- lib/theadmin_issue_hook.rb
+++ lib/theadmin_issue_hook.rb
@@ -1,7 +1,9 @@
 class TheadminIssueHook < Redmine::Hook::ViewListener
   def view_issues_show_details_bottom(context = {})
+    issue = context[:issue]
     content_tag(:tr,
                 content_tag(:td,
-                            "This is on the issue page"))
+                            "This issue is on the #{h issue.project} project and has #{h issue.journals.size} notes",
+                            :colspan => "4"))
   end
 end
